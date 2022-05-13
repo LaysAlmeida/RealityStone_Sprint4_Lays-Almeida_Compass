95e6fcc47f7481d4fefb26c9f38b6c704abf3ecd
Lays Almeida <laysfma@gmail.com>
2022-05-13 15:14:17 -0300

    desafios-ruby

---
+++ OBC-RUBY/Aula 3/for.rb
@@ -0,0 +1,8 @@
+fruits = ['Maçã', 'Uva', 'Morango', 'Tomate']
+count = 1
+
+for x in fruits
+    puts x
+    puts count
+    count = count + 1
+end
---
+++ OBC-RUBY/Aula 4/each_array.rb
@@ -0,0 +1,9 @@
+names = ['Joãozinho', 'Manoel', 'Juca']
+ 
+name = 'Lays Almeida'
+ 
+names.each do |name|
+ puts name
+end
+ 
+puts name
